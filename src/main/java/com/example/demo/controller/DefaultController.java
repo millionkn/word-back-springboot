03eb9321d63afcef6bf8f487136c430223318b5b
millionkn <millionkn@sina.com>
2019-09-07 10:45:23 +0900

    全局404catch住了，静态资源必须放在static文件夹

---
+++ src/main/java/com/example/demo/controller/DefaultController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DefaultController{
+    @RequestMapping(value="")
+    public String index(){
+        return "/static/home_page.html";
+    }
+}
