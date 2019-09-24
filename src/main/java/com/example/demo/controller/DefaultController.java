294a19dfc09a872c390e1520f0a2a3ee71ad958f
millionkn <millionkn@sina.com>
2019-09-24 18:46:17 +0900

    路径问题

--- src/main/java/com/example/demo/controller/DefaultController.java
+++ src/main/java/com/example/demo/controller/DefaultController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class DefaultController{
-    @RequestMapping(value="")
-    public String index(){
-        return "/static/home_page.html";
+public class DefaultController {
+    @RequestMapping(value = "/")
+    public String index() {
+        return "/index.html";
     }
 }
